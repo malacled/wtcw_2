0ef2ad67d628acfd56086cdb99d2e27025f58fb4
malacled <00019257wiut@gmail.com>
2024-03-11 11:52:07 +0500

    pre-api

--- routes/web/home/index.js
+++ routes/web/home/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const home_controller = require('../../../controllers/web/home');
 
-// localhost:3000 (GET REQUEST)
 router.get('/', home_controller.index);
+router.get('/add', home_controller.add);
+router.get('/update', home_controller.update);
 
 module.exports = router;
